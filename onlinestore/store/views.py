93463ee6719f32b3b688a66f6929be225c79283c
andela-jngatia <joan.ngatia@andela.com>
2016-07-26 15:55:01 +0300

    [Feature]Add URl route and view for registration

--- onlinestore/store/views.py
+++ onlinestore/store/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+# from django.shortcuts import render
+from django.views.generic import TemplateView
+# from django.template import RequestContext
 
 # Create your views here.
+# from models import Store, Product
+from forms import RegistrationForm
+
+
+class IndexView(TemplateView):
+    """Landing page view."""
+
+    template_name = 'index.html'
+
+    def get_context_data(self, **kwargs):
+        """Return dictionary representing passed in context."""
+        context = super(IndexView, self).get_context_data(**kwargs)
+        context['registrationform'] = RegistrationForm()
+        # context['loginform'] = LoginForm()
+        return context
+
+
