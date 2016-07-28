bac005fde2f2e8775eaa565236eb46e552559662
andela-jngatia <joan.ngatia@andela.com>
2016-07-28 11:47:51 +0300

    [Chore]Decouple login and register forms.

--- onlinestore/static/js/main.js
+++ onlinestore/static/js/main.js
@@ -1,33 +1,15 @@
-var main = {
-    init: function(){
-        $(".flash-message").fadeOut(4000);
+var main = function(){
+    $(".flash-message").fadeOut(4000);
 
-        $("#loginlink").on("click", function(){
-                $(".register").hide();
-                $(".login").show();
-            });
-
-        $("#registerlink").on("click", function(){
-                $(".register").show();
-                $(".login").hide();
-            });
-    }
-}
-
-// $(document).ready(main);
-// if (window.location.href.indexOf('login') != -1) {
-//     $(".register").hide();
-//     $(".login").show();
-// }
-
-$(document).ready(function() {
-    var url = window.location.href;
-    $(function() {
-        if ( url.indexOf('login') > -1) {
+    $("#loginlink").on("click", function(){
             $(".register").hide();
             $(".login").show();
-        }
-    });
+        });
 
-    main.init();
-});
+    $("#registerlink").on("click", function(){
+            $(".register").show();
+            $(".login").hide();
+        });
+};
+
+$(document).ready(main);
